a697c06414aed8ae33074291def363203f91a1e5
HaozheSun <1002482845@qq.com>
2023-03-26 02:57:20 -0400

    finished part 12

---
+++ e8/part2/part2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <math.h>
+#include <time.h>
+#include "../include/physical.h"
+#include "../include/address_map_arm.h"
+
+#define Clow_freq 261.626
+#define Csharp_freq 277.183
+#define D_freq 293.665
+#define Dsharp_freq 311.127
+#define E_freq 329.628
+#define F_freq 349.228
+#define Fsharp_freq 369.994
+#define G_freq 391.995
+#define Gsharp_freq 415.305
+#define A_freq 440.000
+#define Asharp_freq 466.164
+#define B_freq 493.883
+#define C_freq 523.251
+#define MAX_VOLUME 0x7fffffff
+#define PI 3.1415926
+int main(int argc, char *argv[]){
+    int fd=-1;
+    void *LW_virtual;
+    volatile int *audio_virtule;
+    fd = open_physical (fd);
+	LW_virtual = map_physical(fd, LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
+    audio_virtule = (int *)(LW_virtual + AUDIO_BASE);
+    float notes[13] = {Clow_freq, Csharp_freq, D_freq, Dsharp_freq, 
+                        E_freq, F_freq, Fsharp_freq, G_freq, Gsharp_freq,
+                        A_freq, Asharp_freq, B_freq, C_freq};
+    *audio_virtule |= 0x8;
+    *audio_virtule &= ~0xB;
+
+    char * input;
+    input = argv[1];
+    int i=0;
+    int node = 0;
+    int mixTone[13];
+    if (strlen(input) != 13){
+        printf("The input is wrong, plaese input 13 0/1s");
+        return 0;
+    }
+    for (i = 0; i < strlen(input); i++){
+        if (input[i] == '1'){
+            node+=1;
+            mixTone[i] = 1;
+        }
+        else{
+            mixTone[i] = 0;
+        }
+    }
+    int volume=MAX_VOLUME/node;
+    int j=0;
+    int sound=0;
+    for (j=0; j<8000; j++){
+        while ((((*(audio_virtule + 1) >> 16) & 0xFF) == 0x00) || (((*(audio_virtule + 1) >> 24) & 0xFF) == 0x00));
+        sound=0;
+        for (i=0; i<13; i++){
+            if (mixTone[i] == 1){
+                sound+=(int)volume * sin(j * 2* PI * notes[i] / 8000);
+            }
+        }
+        *(audio_virtule + 2) = sound;
+        *(audio_virtule + 3) = sound;
+        
+    }
+    
+    unmap_physical (LW_virtual, LW_BRIDGE_SPAN);
+	close_physical(fd);
+
+    return 0;
+}
